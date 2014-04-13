7d3be0106ba9607441f72cf768b2384f68790d49
waselrosa <rosa@waselrosa.com>
2014-04-12 19:03:27 -0700

    API request set up

--- js/main.js
+++ js/main.js
@@ -1,29 +1,42 @@
 $('#send').click(function(event) {
 	event.preventDefault();
 
-	appendUsersTextToTheDom();
-	//empty out the input box
-	$('#new-request').val('');
-});
-
-$('#translations').on("DOMSubtreeModified", function(){
-	$('.row').fadeIn();
-})
-
-function appendUsersTextToTheDom() {
 	//Get the text the user entered
 	var newQuestion = $('#new-request').val();
 
 	//Get the template 
 	var questionRac = $('#raccoonQ-template').html();
 
+	appendUsersTextToTheDom(newQuestion, questionRac);
+
+	//empty out the input box
+	$('#new-request').val('');
+	
+	translate(newQuestion);
+});
+
+$('#translations').on("DOMSubtreeModified", function(){
+	$('.row').fadeIn();
+})
+
+function appendUsersTextToTheDom(newQuestion, template) {
 	//Append the user input to DOM
-	var $questionRac = $(questionRac);
-	$questionRac.find('.howzm-box').text(newQuestion);
-	$('#translations').append($questionRac);
+	var $template = $(template);
+	$template.find('.howzm-box').text(newQuestion);
+	$('#translations').append($template);
 }
 
+function translate(newQuestion) {
+	var url = "http://www.translatefun.com/pidginenglish";
+	var data = { q: newQuestion };
 
+	$.getJSON(url, data, function(res) {
+		var answerRooster = $('#roosterA-template').html();
+
+		appendUsersTextToTheDom(res.text, answerRooster);
+		
+	})
+}
 
 
 
